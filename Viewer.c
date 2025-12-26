e7ce95b3536a96e5a32fdbba0b717b1f66f6bbc4
Albion Qerreti <albionqerreti221b@gmail.com>
2025-12-26 19:03:59 +0100

    Changed the structure use only SDL lib

--- Viewer.c
+++ Viewer.c
@@ -1,9 +1,8 @@
+#include <SDL2/SDL_render.h>
 #include <SDL2/SDL_surface.h>
-#include <cairo/cairo.h>
-#include <stdio.h>
-#include <gtk/gtk.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <SDL2/SDL_video.h>
 
 
 struct Dimension {
@@ -11,73 +10,56 @@ struct Dimension {
     int height;
 };
 
-static void activate (GtkApplication *app, gpointer user_data)
-{
-  GtkWidget *window;
-
-  struct Dimension *dim = (struct Dimension*)user_data;
-
-  window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW (window), "Bubii Viewer");
-  gtk_window_set_default_size (GTK_WINDOW (window), dim->width, dim->height);
-  gtk_window_present (GTK_WINDOW (window));
-}
-
-void draw_pixel(SDL_Surface *surface, int x, int y, unsigned char r, unsigned char g, unsigned char b) {
-    int offset = y * surface->pitch + x * surface->format->BytesPerPixel;
-    unsigned char *pixels = (unsigned char *)surface->pixels;
-    pixels[offset + 0] = r;
-    pixels[offset + 1] = g;
-    pixels[offset + 2] = b;
-}
-
-void convert_image_to_pixels(const char *path){
-    SDL_Surface *surface = IMG_Load(path);
-    if (!surface) {
-        printf("IMG_Load error: %s\n", IMG_GetError());
-        return;
-    }
-    
-    printf("Loaded image %dx%d\n", surface->w, surface->h);
-    SDL_LockSurface(surface);
-
-    int width = surface -> w;
-    int height = surface->h;
-    int pitch = surface->pitch;
-    int bytes_per_pixel = surface->format->BytesPerPixel;
-    unsigned char *pixels = (unsigned char *)surface->pixels;
-    
-    for (int i = 0; i < surface->h; i++) {
-        for (int j = 0; j < surface -> w; j++) {
-            int offset = i*pitch + j*bytes_per_pixel;
-            unsigned char r = pixels[offset + 0];
-            unsigned char g = pixels[offset + 1];
-            unsigned char b = pixels[offset + 2];    
-            // printf("offset : %d", r);
-        }
-    }
-}
-
-
 
 
 
 
 int main(int argc, char **argv){
-    GtkApplication *app;
-    //if fail or success
-    int status;
-    struct Dimension dim = {1000, 1000};
-
-
-    app = gtk_application_new ("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
-    g_signal_connect (app, "activate", G_CALLBACK (activate), &dim);
+    SDL_Init(SDL_INIT_VIDEO);
+    IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG);
     
     char *path = "/home/albionii/Desktop/357479338_6387107201378091_6123221653863258753_n.jpg";
-    convert_image_to_pixels(path);
+    SDL_Surface * surface = IMG_Load(path);
+    
+    if (!surface) {
+        printf("Loading image failed");
+        return 1;
+    }
+    
+    SDL_Window *window = SDL_CreateWindow(
+        "My Image Viewer",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        surface->w,
+        surface->h,
+        0
+    );
+    
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+    
+    SDL_FreeSurface(surface);
+    
+    SDL_Event e;
+    int running = 1;
 
-    status = g_application_run (G_APPLICATION (app), argc, argv);
-    g_object_unref (app);
+    while (running) {
+        //This loop is non-blocking and when an event happens it checks if I clicked "X" button.
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT)
+                running = 0;
+        }
+    
+            SDL_RenderClear(renderer);
+            SDL_RenderCopy(renderer, texture, NULL, NULL);
+            SDL_RenderPresent(renderer);
+    }
+    
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    IMG_Quit();
+    SDL_Quit();
 
-    return status;
+    return 0;
 }
